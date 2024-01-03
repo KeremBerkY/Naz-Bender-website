39c5364d15aa2cf361e54d03d47fd9c12e211ae1
kby <keremberkyumlu@gmail.com>
2024-01-03 17:25:11 +0300

    new project added Naz-Bender-website

---
+++ naz.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// ÖRN; DOM
+// const hiddenMenu = document.querySelector(".hidden-menu");
+// const overlay = document.querySelector(".overlay");
+// const btnCloseModal = document.querySelector(".btn--close-modal");
+// const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+// // FUNCTIONS
+// const openModal = function (e) {
+//   e.preventDefault();
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// };
+
+// const closeModal = function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
